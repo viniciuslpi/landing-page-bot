fd6c5fe755679a6452a85fefaa3e07a28771d1fc
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-28 11:17:42 -0300

    scroll element menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+  }
+
+  scrollToElement($element: string): void {
+    this.viewportScroller.scrollToAnchor($element);
   }
 
 }
