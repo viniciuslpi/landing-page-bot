357d35a5114f23ee8781555334230364c63d66ec
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-06-23 15:06:25 -0300

    new style menu

--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,22 +1,20 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
   public linkWhatsApp: String = "https://wa.me/5511992906137";
-
   constructor(private viewportScroller: ViewportScroller) { }
-
-  ngOnInit(): void {
-  }
 
   scrollToElement($element: string): void {
     this.viewportScroller.scrollToAnchor($element);
   }
 
 }
+
+
